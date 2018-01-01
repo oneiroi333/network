16743b48dc6f8e40789d52758913411d719daed3
morpheus <morpheus@alcatraz.com>
2018-01-01 18:01:09 +0100

    ipv6 works now

--- gethostip.c
+++ gethostip.c
@@ -8,16 +8,14 @@
 #include <unistd.h>
 #include <string.h>
 
-#define NOT_INIT '.'
+void print_result(struct addrinfo *result);
 
 int
 main(int argc, char *argv[])
 {
 	struct addrinfo hints;
-	struct addrinfo *result, *rp;
+	struct addrinfo *result;
 	int status;
-	char ipv4_addr[INET_ADDRSTRLEN] = {NOT_INIT};
-	char ipv6_addr[INET6_ADDRSTRLEN] = {NOT_INIT};
 
 	if (argc < 2) {
 		fprintf(stderr, "Usage: %s hostname\n", argv[0]);
@@ -25,44 +23,85 @@ main(int argc, char *argv[])
 	}
 
 	memset(&hints, 0, sizeof(struct addrinfo));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = 0;
-	hints.ai_flags = 0;
-	hints.ai_protocol = 0;
 
+	/* Lookup IPv4 */
+	hints.ai_family = AF_INET;
 	status = getaddrinfo(argv[1], NULL, &hints, &result);
 	if (status != 0) {
-		fprintf(stdout, "Sry, could not lookup: %s\n", argv[1]);
-		exit(EXIT_FAILURE);
+		fprintf(stderr, "[Error IPv4]: %s\n", gai_strerror(status));
+	} else {
+		print_result(result);
 	}
 
-	for (rp = result; rp != NULL; rp = rp->ai_next) {
-		/* IPv4 */
-		if (rp->ai_family == AF_INET) {
-			if (ipv4_addr[0] == NOT_INIT) {
-				inet_ntop(AF_INET, &(((struct sockaddr_in *) rp->ai_addr)->sin_addr), ipv4_addr, INET_ADDRSTRLEN);
-			}
-		}
-		/* IPv6 */
-		if (rp->ai_family == AF_INET6) {
-			if (ipv6_addr[0] == NOT_INIT) {
-				inet_ntop(AF_INET6, &(((struct sockaddr_in6 *) rp->ai_addr)->sin6_addr), ipv6_addr, INET6_ADDRSTRLEN);
-			}
-		}
-		/* Found both ipv4 and ipv6 addresses */
-		if (ipv4_addr[0] != NOT_INIT && ipv6_addr[0] != NOT_INIT) {
-			break;
-		}
-	}
-
-	if (ipv4_addr[0] != NOT_INIT) {
-		fprintf(stdout, "IPv4: %s\n", ipv4_addr);
-	}
-	if (ipv6_addr[0] != NOT_INIT) {
-		fprintf(stdout, "IPv6: %s\n", ipv6_addr);
+	/* Lookup IPv6 */
+	hints.ai_family = AF_INET6;
+	status = getaddrinfo(argv[1], NULL, &hints, &result);
+	if (status != 0) {
+		fprintf(stderr, "[Error IPv6]: %s\n", gai_strerror(status));
+	} else {
+		print_result(result);
 	}
 
 	freeaddrinfo(result);
 
 	exit(EXIT_SUCCESS);
+}
+
+void
+print_result(struct addrinfo *result)
+{
+	struct addrinfo *rp;
+	struct in_addr *addr;
+	struct in6_addr *addr6;
+	char ipv4_addr[INET_ADDRSTRLEN];
+	char ipv6_addr[INET6_ADDRSTRLEN];
+	static int first = 0;
+	static int first6 = 0;
+
+
+	for (rp = result; rp != NULL; rp = rp->ai_next) {
+		switch(rp->ai_family) {
+			case AF_INET:
+				++first;
+				addr = &(((struct sockaddr_in *) rp->ai_addr)->sin_addr);
+				inet_ntop(AF_INET, addr, ipv4_addr, INET_ADDRSTRLEN);
+				
+				if (first == 1) {
+					fprintf(stdout, "===============[ IPv4 ]===============\n");
+					fprintf(stdout, "Address: %s\n", ipv4_addr);
+				}
+				break;
+			case AF_INET6:
+				++first6;
+				addr6 = &(((struct sockaddr_in6 *) rp->ai_addr)->sin6_addr);
+				inet_ntop(AF_INET6, addr6, ipv6_addr, INET6_ADDRSTRLEN);
+
+				if (first6 == 1) {
+					fprintf(stdout, "===============[ IPv6 ]===============\n");
+					fprintf(stdout, "Address: %s\n", ipv6_addr);
+				}
+				break;
+			default:
+				;
+		}
+		if (first == 1 || first6 == 1) {
+			fprintf(stdout, "Socket type: ");
+		} else {
+			fprintf(stdout, ", ");
+		}
+		switch(rp->ai_socktype) {
+			case SOCK_STREAM:
+				fprintf(stdout, "TCP");
+				break;
+			case SOCK_DGRAM:
+				fprintf(stdout, "UDP");
+				break;
+			case SOCK_RAW:
+				fprintf(stdout, "RAW");
+				break;
+			default:
+				fprintf(stdout, "OTHER");
+		}
+	}
+	fprintf(stdout, "\n");
 }
